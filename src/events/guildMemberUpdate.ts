f4f392c4de9541af3bc90920e6bcffbaa0f01b83
RonPoly <ronald@polynize.io>
2025-07-20 23:40:31 -0400

    fix role sync thrashing

--- src/events/guildMemberUpdate.ts
+++ src/events/guildMemberUpdate.ts
@@ -1,11 +1,11 @@
 import { Client, Events } from 'discord.js';
-import { syncMemberRoles } from '../utils/role-sync';
+import { syncUserRoles } from '../utils/role-sync';
 import { getGuildConfig } from '../utils/guild-config';
 
 export default function registerGuildMemberUpdate(client: Client) {
   client.on(Events.GuildMemberUpdate, async (_, newMember) => {
     const config = await getGuildConfig(newMember.guild.id);
     if (!config || !config.warmane_guild_name || !config.member_role_id) return;
-    await syncMemberRoles(newMember);
+    await syncUserRoles(newMember);
   });
 }
